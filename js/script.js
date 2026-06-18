284dd698434ebddf556c3fc3f7b046a9049df1fc
David Soares <137106233+Soa1804@users.noreply.github.com>
2026-06-18 07:29:55 +0000

    concertando o script para voltar ao index

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const openBtn = document.getElementById('openBtn');
     const envelope = document.querySelector('.floating');
 
+    // Corrige quando voltar pela seta do navegador
+    window.addEventListener('pageshow', () => {
+
+        envelope.style.transition = 'none';
+        envelope.style.transform = '';
+        envelope.style.opacity = '';
+
+        // força o navegador a aplicar as mudanças
+        void envelope.offsetWidth;
+
+        envelope.style.transition = '';
+
+    });
+
     openBtn.addEventListener('click', () => {
 
         const ripple = document.createElement('div');
@@ -28,15 +42,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
         });
 
+        envelope.style.transition =
+            'all 1s cubic-bezier(0.4, 0, 0.2, 1)';
         envelope.style.transform = 'scale(1.2) translateY(-200px)';
         envelope.style.opacity = '0';
 
-        envelope.style.transition =
-            'all 1s cubic-bezier(0.4, 0, 0.2, 1)';
-        
         setTimeout(() => {
             window.location.href = 'proposta.html';
         }, 1000);
+
     });
 
 });
